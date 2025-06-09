00802492c4f555f921313d0496338bf301198217
Rafael Sobral <rafaelsobraldelimaalves@gmail.com>
2025-06-08 23:45:12 -0300

    Add files via upload

--- blog/urls.py
+++ blog/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blogapp.urls'))
+    path('blog/', include('blogapp.urls')),
+    path('', lambda request: redirect('blog/home/')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
